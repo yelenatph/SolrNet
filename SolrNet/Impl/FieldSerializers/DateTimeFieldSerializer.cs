fdfcfec52560a70f0ff17d9ea897f18159c4920e
Yelena <jelena@thepaymenthouse.com>
2018-09-06 14:04:14 +0300

    added name of branch to commit

--- SolrNet/Impl/FieldSerializers/DateTimeFieldSerializer.cs
+++ SolrNet/Impl/FieldSerializers/DateTimeFieldSerializer.cs
@@ -17,7 +17,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-//my comment
+//my comment from TestDocumentation
 namespace SolrNet.Impl.FieldSerializers {
     /// <summary>
     /// Serializes datetime fields
